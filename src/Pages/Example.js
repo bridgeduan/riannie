c6500e35d052385f97e0108668733639f727d1c7
Lian Duan <bridgeduan@LiandeMacBook-Pro.local>
2024-08-09 10:34:57 -0700

    adding things

--- src/Pages/Example.js
+++ src/Pages/Example.js
@@ -4,12 +4,19 @@ import "./Example.css"
 
 import Navbar from "../Navbar";
 
+import g2 from "../Group2.jpg"
+import g3 from "../Group 3.png"
+import g1 from "../Group 1 .png"
+
 export default function Example(){
     return(
         <>
             <Navbar />
             <div id="example">
-        
+                <img className="example_group" src ={g2}/>
+                <img className="example_group" src ={g3}/>
+                <img className="example_group" src ={g1}/>
+
             </div>
         </>
     )
